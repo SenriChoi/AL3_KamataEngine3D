c4f4a50aa1353b7a76f5525b2dc45bd8a2a6e45d
ジョカエイ <147121578+SenriChoi@users.noreply.github.com>
2024-04-18 13:08:37 +0900

    ライン描画

--- DirectXGame/scene/GameScene.h
+++ DirectXGame/scene/GameScene.h
@@ -39,10 +39,23 @@ public: // メンバ関数
 	/// </summary>
 	void Draw();
 
+
 private: // メンバ変数
 	DirectXCommon* dxCommon_ = nullptr;
 	Input* input_ = nullptr;
 	Audio* audio_ = nullptr;
+
+	uint32_t textureHandle_ = 0;
+	uint32_t soundDateHandle_ = 0;
+	uint32_t voiceHandle_ = 0;
+
+	Sprite* sprite_ = nullptr;
+
+	Model* model_ = nullptr;
+	float inputFloat3[3] = {0, 0, 0};
+	
+	WorldTransform worldTransform_;
+	ViewProjection viewProjection_;
 
 	/// <summary>
 	/// ゲームシーン用
